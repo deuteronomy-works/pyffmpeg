322fe7062719a764327c773fc2f4c092878a8669
amoh-godwin <amohgyebigodwin@gmail.com>
2020-03-25 15:59:37 +0000

    use OS specific ffmpeg executable

--- pyffmpeg/__init__.py
+++ pyffmpeg/__init__.py
@@ -4,6 +4,16 @@ Created on Wed Mar 25 15:07:19 2020
 
 """
 
+import os
+from platform import system
+from base64 import b64decode
+if system() == 'Windows':
+    from .static.bin.win32 import win32
+elif system == 'linux':
+    from .static.bin.linux import linux
+else:
+    from .static.bin.darwin import darwin
+
 class FFmpeg():
 
 
@@ -12,4 +22,25 @@ class FFmpeg():
 
 
     def __init__(self):
-        pass
+
+        cwd = os.path.dirname(__file__)
+
+        # Load OS specific ffmpeg executable
+        if system() == 'Windows':
+            self.path_to_ffmpeg = cwd + './static/bin/win32'
+            ffmpeg_file = self.path_to_ffmpeg + '/ffmpeg.exe'
+            b64 = win32.contents
+        elif system == 'linux':
+            self.path_to_ffmpeg = cwd + './static/bin/linux'
+            ffmpeg_file = self.path_to_ffmpeg + '/ffmpeg'
+            b64 = linux.contents
+        elif system == 'darwin':
+            self.path_to_ffmpeg = cwd + './static/bin/darwin'
+            ffmpeg_file = self.path_to_ffmpeg + '/ffmpeg'
+            b64 = darwin.contents
+        else:
+            b64 = ""
+
+        raw = b64decode(b64)
+        with open(ffmpeg_file, 'wb') as f:
+            f.write(raw)
