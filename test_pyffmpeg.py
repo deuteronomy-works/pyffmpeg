44a572a41f3c1433b7050c551edb23d5842e4a0f
amoh-godwin <amohgyebigodwin@gmail.com>
2020-03-27 12:11:54 +0000

    solve #1

--- test_pyffmpeg.py
+++ test_pyffmpeg.py
@@ -2,17 +2,26 @@ import pytest
 from pyffmpeg import FFmpeg
 
 def test_main():
+
     """
     """
-    if FFmpeg():
-        assert True
+
+    # Test to see if save directory is used
+    sav_dir = 'H:\\Ulala'
+    ffmpeg = FFmpeg(sav_dir)
+    if ffmpeg:
+        assert ffmpeg.save_dir == sav_dir
+    else:
+        assert False
 
 def test_convert():
+
     """
     """
-    a = FFmpeg()
+
     i = 'H:\\GitHub\\pyffmpeg\\_test\\f.mp3'
     o = 'H:\\GitHub\\pyffmpeg\\_test\\f.wav'
 
+    a = FFmpeg()
     ret = a.convert(i, o)
     assert ret == o
