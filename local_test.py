17228f32abbd2fbf1bb0e9ea25f7bdc11ac17fe1
amoh-godwin <amohgyebigodwin@gmail.com>
2020-08-18 18:50:04 +0000

    Update local_test.py

--- local_test.py
+++ local_test.py
@@ -3,5 +3,5 @@ from pyffmpeg import FFmpeg
 
 ff = FFmpeg()
 out = ff.convert('H:\\GitHub\\pyffmpeg\\_test\\f.mp3', 'H:\\GitHub\\pyffmpeg\\_test\\f.wav')
-fps = ff.get_fps("H:/CS/practice/python/pyffmpeg subproces/vv.mp4")
+fps = ff.get_fps("H:/CS/practice/python/pyffmpeg subproces/vid.mp4")
 print(out, fps)
