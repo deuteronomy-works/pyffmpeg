55dae1e4e85a6114ae7c0d75a97ac39f8c40e1a8
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2020-10-20 00:55:12 +0000

    Alpha release 2.0a0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as rm:
 
 setup(
     name='pyffmpeg',
-    version='1.8.1',
+    version='2.0a0',
     description='FFmpeg wrapper for python',
     long_description=long_desc,
     long_description_content_type='text/markdown',
@@ -22,6 +22,8 @@ setup(
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
-    keywords='ffmpeg, FFmpeg, audio',
+    keywords='ffmpeg, FFmpeg, ffprobe, FFprobe'
+    keywords += ', album art, cover art, metadata'
+    keywords += ', conversion, converting, audio, video',
     packages=find_packages(),
 )
