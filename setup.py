eb947881a7078091f099286d7c84314e4851bfa5
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2020-10-20 01:28:23 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as rm:
 
 setup(
     name='pyffmpeg',
-    version='2.0a0',
+    version='2.0',
     description='FFmpeg wrapper for python',
     long_description=long_desc,
     long_description_content_type='text/markdown',
