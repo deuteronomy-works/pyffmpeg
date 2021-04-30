3a6b88920e0f953085e354711830d377b09d2f02
amoh-godwin <amohgyebigodwin@gmail.com>
2021-04-30 08:20:49 +0000

    find codec name

--- pyffmpeg/extract_functions.py
+++ pyffmpeg/extract_functions.py
@@ -1,5 +1,14 @@
 import re
 
+
+def _codec_name(line):
+    if 'Video:' in line:
+        cod = re.findall(r'Video: .*? ', line)[0]
+        name = cod.split(':')[1].strip()
+        name_string = 'codec: ' + name
+
+    return [name_string]
+
 def _fps(line):
     if 'fps' in line:
         fps = re.findall(r'\d+.?\d* fps', line)[0].split(' fps')[0]
@@ -8,4 +17,4 @@ def _fps(line):
     return [fps_str]
 
 
-FUNC_LIST = [_fps]
+FUNC_LIST = [_codec_name, _fps]
