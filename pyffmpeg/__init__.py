9111891a6d055ee3501497a940c03c9f1792c37f
amoh-godwin <amohgyebigodwin@gmail.com>
2022-02-11 04:13:57 +0000

    fix pep

--- pyffmpeg/__init__.py
+++ pyffmpeg/__init__.py
@@ -84,8 +84,8 @@ class FFmpeg():
             self._in_duration = float(d)
             self.monitor(out)
 
-        outP = Popen(options, shell=SHELL, 
-            stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        outP = Popen(
+            options, shell=SHELL, stdin=PIPE, stdout=PIPE, stderr=PIPE)
         self._ffmpeg_instances['convert'] = outP
         self.error = str(outP.stderr.read(), 'utf-8')
         return out
