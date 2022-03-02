7a3501d9ba0871e1dd4b9d069392d92be320570e
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2022-03-02 20:09:06 +0000

    Create push_to_branches.py

---
+++ push_to_branches.py
@@ -0,0 +1,27 @@
+import sys
+import os
+from time import sleep
+
+
+_, token = sys.argv
+
+cwd = os.path.realpath('.')
+
+# Login to GH
+with open('token.txt', 'w') as tok:
+    tok.write(token)
+    print('Finished writing token file')
+
+cmd = 'gh auth login --with-token < token.txt'
+os.system(cmd)
+print('Authenticated')
+
+
+BRANCHES = ('build-darwin', 'build-linux', 'build-windows')
+
+for branch in BRANCHES:
+    cmd1 = f'gh pr create --base {branch}'
+    os.system(cmd1)
+
+
+print('All Done')
