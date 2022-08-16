b69596664b4a14df7a05dec816e4c7fd64330983
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2022-08-16 12:07:38 +0000

    pep8

--- tests/test_pseudo_ffprobe.py
+++ tests/test_pseudo_ffprobe.py
@@ -1,8 +1,15 @@
 import pytest
 from pyffmpeg import FFprobe
+
 # test speed to make sure no convertion took place
 # test file exist does not happen
-TEST_FILES = {"tests/countdown.mp4", "tests/Easy_Lemon_30_Second_-_Kevin_MacLeod.mp3", "tests/Ecossaise in E-flat - Kevin MacLeod.mp3"}
+
+
+TEST_FILES = {
+    "tests/countdown.mp4",
+    "tests/Easy_Lemon_30_Second_-_Kevin_MacLeod.mp3",
+    "tests/Ecossaise in E-flat - Kevin MacLeod.mp3"}
+
 
 def test_probe():
     for file in TEST_FILES:
