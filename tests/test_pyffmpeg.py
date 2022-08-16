87cb2a1617035af83d54228bfaf1a6fd7659c3e1
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2022-08-16 13:46:12 +0000

    improve tests

--- tests/test_pyffmpeg.py
+++ tests/test_pyffmpeg.py
@@ -7,16 +7,10 @@ from pyffmpeg.misc import Paths
 
 cwd = os.path.dirname(__file__)
 
-i = "https://raw.githubusercontent.com/"
-i += "deuteronomy-works/pyffmpeg/master/tests/f.mp3"
-
-TEST_FOLDER = "https://raw.githubusercontent.com/"
+TEST_FOLDER = "http://raw.githubusercontent.com/"
 TEST_FOLDER += "deuteronomy-works/pyffmpeg/master/tests/"
 
-# EASY_LEMON = os.path.join(
-#    cwd, 'tests', 'Easy_Lemon_30_Second_-_Kevin_MacLeod.mp3')
 EASY_LEMON = TEST_FOLDER + 'Easy_Lemon_30_Second_-_Kevin_MacLeod.mp3'
-
 E_FLAT = TEST_FOLDER + "Ecossaise in E-flat - Kevin MacLeod.mp3"
 
 
