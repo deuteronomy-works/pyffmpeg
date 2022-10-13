e4ae0c730163daf8ff0fbbe7265aef327bb3b7dc
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2022-10-13 03:36:00 +0000

    Update rename.py

--- rename.py
+++ rename.py
@@ -10,11 +10,11 @@ lists = glob('./dist/*.whl')
 oss = {
     'win32': 'py3-none-win32.whl',
     'amd64': 'py3-none-win_amd64.whl',
-    'linux_intel': "py3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
-    'linux_aarch': "py3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl",
-    'macos11': "py3-macosx_11_0_arm64.whl",
-    'macos_intel': "py3-macosx_10_9_x86_64.whl",
-    'macos_uni': "py3-macosx_10_9_universal2.whl"}
+    'linux_intel': "py3-none-manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+    'linux_aarch': "py3-none-manylinux_2_17_aarch64.manylinux2014_aarch64.whl",
+    'macos11': "py3-none-macosx_11_0_arm64.whl",
+    'macos_intel': "py3-none-macosx_10_9_x86_64.whl",
+    'macos_uni': "py3-none-macosx_10_9_universal2.whl"}
 
 for x in lists:
     if '-any' in x:
