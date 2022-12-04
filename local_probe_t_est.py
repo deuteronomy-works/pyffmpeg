be960c2c31a9ceff610cd211f27e9588abe06d38
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2022-12-04 10:11:21 +0000

    Update local_probe_t_est.py

--- local_probe_t_est.py
+++ local_probe_t_est.py
@@ -3,7 +3,9 @@ from pyffmpeg import FFprobe
 
 
 test_folder = os.path.abspath('./tests/')
-test_file = os.path.join(test_folder, 'loss_countdown.mkv')
+test_file = os.path.join(test_folder, 'Easy_Lemon_30_Second_-_Kevin_MacLeod.mp3')
+# test_file = "G:\Entertainment\Movies\[PFM].Hacksaw.Ridge.2016.720p.BrRip.x265.HEVCBay.mkv"
+#test_file = "G:\Entertainment\Movies\Interstellar (2014)\Interstellar.2014.720p.BluRay.x264.YIFY.mp4"
 
 f = FFprobe(test_file)
 # ret = f.get_album_art('cover.png')
